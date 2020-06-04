9d78cf3f4a657fa5e64326bb5641204c5684441d
Kanako Baba <63246382+canathub@users.noreply.github.com>
2020-06-05 00:19:50 +0900

    Update Webser2.java

--- src/kadai/baba/Webser2.java
+++ src/kadai/baba/Webser2.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/kadai.baba/Webser2")
+@WebServlet("/WebService/src/kadai/baba/Webser2")
 public class Webser2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
